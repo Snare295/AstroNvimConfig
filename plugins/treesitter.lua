87dff119c0b97b69a9b35b240cdb942143d2cea1
Snare295 <vladneum@gmail.com>
2024-02-07 20:36:50 +0200

    add langs to treesitter

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,23 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    -- ensure_installed = { "lua" },
+     auto_install = true,
+     ensure_installed = { 
+      "lua",
+      "cpp",
+      "kotlin",
+      "java",
+      "c_sharp",
+      "dockerfile",
+      "dart",
+      "c",
+      "python",
+      "glsl",
+      "go",
+      "gitignore",
+      "yaml",
+      "javascript",
+      "typescript",
+    },
   },
 }
